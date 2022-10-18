59824b201742856f6fe09c628618a282d2be056b
taffalyousef <youseftaffal@mail.polimi.it>
2022-10-18 22:13:25 +0200

    Update sketch.js
    Assignment 02

--- sketch.js
+++ sketch.js
@@ -1,16 +1,91 @@
-function preload() {
-	// put preload code here
-}
-
+let shapesWidth, colors, randomColor;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  colors = [color(207, 212, 163), color(56, 93, 128)]
+
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  background(0, 0, 0);
+  frameRate(30);
+  noFill();
+  strokeWeight(3);
+
 }
 
 function draw() {
-	// put drawing code here
+  randomColor = lerpColor(colors[0], colors[1], random());
+  shapesWidth = width / 6;
+  const ampOfRotation = 360;
+  const numberOfrectangles = 10; 
+
+  drawRectangles(numberOfrectangles, ampOfRotation, 10,shapesWidth, height/6, randomColor)
+  drawRectangles(numberOfrectangles, ampOfRotation, 10, width - shapesWidth, height - shapesWidth, randomColor)
+
+  if(frameCount <= 270){
+    drawEllipse(shapesWidth, randomColor)
+  } else if (frameCount >= 270 && frameCount < 600){
+    rotateRectangles(randomColor)
+  } else if (frameCount >=600){
+    frameCount = 0
+  }
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseMoved() {
+  // draws small circles whenever the mouse moves 
+  drawCircle(mouseX, mouseY, randomColor)
+}
+ 
+function drawCircle(posX, posY, shapeColor) {
+  push();
+    fill(shapeColor);
+    strokeWeight(1);  
+    translate(posX, posY)
+    circle(0, 0, 10);
+  pop();
+}
+
+function drawEllipse(shapeWidth, shapeColor){
+  push();
+  translate(width / 2, height / 2); 
+  background(0, 0, 0, 17);
+  for (let i = 0; i < 10; i++) {
+      push();
+        rotate(i * 360 / 10);
+        stroke(shapeColor);
+        ellipse(cos(frameCount) * shapeWidth, sin(frameCount), cos(frameCount)* shapeWidth)
+      pop();
+  }
+  pop();
+}
+
+// draws multiples rectangles with different sizes
+function drawRectangles(counter, ampOfRotation, side, x, y, shapeColor){
+  push();
+    translate(x, y);
+    stroke(shapeColor);
+    for (i = 0; i < counter; i++) {
+      let angle = frameCount + i * 5; 
+      let rotation = sin(angle) * ampOfRotation; 
+      push();
+      rotate(rotation);
+      rect(0, 0, side * i, side * i);
+      pop();
+    }
+  pop();
+}
+
+// draws the middle circle of rectangles 
+function rotateRectangles(shapeColor){
+  push();
+    translate(width / 2, height / 2)
+    rectMode(CENTER);
+    stroke(shapeColor);
+    noFill();
+    rotate(frameCount/2)
+    rect (0, 0, width/4, height/4);
+  pop();
 }
